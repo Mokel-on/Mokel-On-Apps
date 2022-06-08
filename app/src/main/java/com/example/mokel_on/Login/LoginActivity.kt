2221d7c695a8bb7cd4454fb4baf686e70017b61a
Claren clarichie <clarenclarichie8@gmail.com>
2022-06-08 22:09:58 +0700

    push 1

--- app/src/main/java/com/example/mokel_on/Login/LoginActivity.kt
+++ app/src/main/java/com/example/mokel_on/Login/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.example.mokel_on.Login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.example.mokel_on.databinding.ActivityLoginBinding
+import com.example.mokel_on.home.HomeActivity
 import com.example.mokel_on.register.RegistActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -17,15 +23,54 @@ class LoginActivity : AppCompatActivity() {
 
         supportActionBar!!.hide()
 
+        auth = FirebaseAuth.getInstance()
+
         binding.txtButton1.setOnClickListener{
             val intent = Intent(this@LoginActivity,RegistActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.logEmail.text.toString()
+            val password = binding.loginPw.text.toString()
+
+            if (email.isEmpty()) {
+                binding.logEmail.error = "Required!"
+                binding.logEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.logEmail.error = "Email doesn't match"
+                binding.logEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                binding.loginPw.error = "Required!"
+                binding.loginPw.requestFocus()
+                return@setOnClickListener
+            }
+            LoginFirebase(email, password)
         }
 
 
 
 
 
+
+
+    }
+
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Login Completed", Toast.LENGTH_SHORT).show()
+                    intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
 
 
     }
--- app/src/main/java/com/example/mokel_on/register/RegistActivity.kt
+++ app/src/main/java/com/example/mokel_on/register/RegistActivity.kt
@@ -6,15 +6,19 @@ import android.os.Bundle
 import com.example.mokel_on.Login.LoginActivity
 import com.example.mokel_on.R
 import com.example.mokel_on.databinding.ActivityRegistBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistActivity : AppCompatActivity() {
 
+    lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityRegistBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         supportActionBar!!.hide()
 
