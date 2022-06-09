981727f2bb1029c8481152dd30dbe2a35251fc6f
Claren clarichie <clarenclarichie8@gmail.com>
2022-06-09 15:26:58 +0700

    background splash screen

--- app/src/main/java/com/example/mokel_on/home/HomeActivity.kt
+++ app/src/main/java/com/example/mokel_on/home/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ImageSwitcher
 import android.widget.ImageView
 import com.example.mokel_on.R
 import com.example.mokel_on.databinding.ActivityHomeBinding
+import kotlinx.android.synthetic.main.list.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -21,7 +22,7 @@ class HomeActivity : AppCompatActivity() {
         val imgswitcher = findViewById<ImageSwitcher>(R.id.switcher_1)
         imgswitcher?.setFactory {
             val imgView = ImageView(applicationContext)
-            imgView.scaleType = ImageView(applicationContext)
+            imgView.scaleType = ImageView.ScaleType.FIT_CENTER
             imgView.setPadding(8, 8, 8, 8)
             imgView
         }
