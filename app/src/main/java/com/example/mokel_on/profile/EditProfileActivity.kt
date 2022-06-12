278eb2ba6097e33f3cdcf64a6bb24fb450c97767
painanass <inassiregar0@gmail.com>
2022-06-12 20:07:49 +0700

    act home

---
+++ app/src/main/java/com/example/mokel_on/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mokel_on.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mokel_on.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
